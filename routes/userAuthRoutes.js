41d7c38ed922e6c6a5a4947bb2959374a9f08131
affaqkhan211 <affaqkhan211@gmail.com>
2023-09-23 21:12:03 +0500

    user auth done

--- routes/userAuthRoutes.js
+++ routes/userAuthRoutes.js
@@ -6,8 +6,8 @@ import isAuthenticated from "../middlewares/auth.js";
 userAuthRouter.post("/signup", userSignup);
 userAuthRouter.post("/login", login);
 // userAuthRouter.post('/:userId/verify', sendVerificationCode);
-userAuthRouter.post('/:userId/add-address', isAuthenticated , addAddress);
-userAuthRouter.post('/:userId/set-pin', isAuthenticated,  setPin);
-userAuthRouter.post('/:userId/verify-pin', isAuthenticated,  verifyPin);
+userAuthRouter.post('/:userId/add-address', addAddress);
+userAuthRouter.post('/:userId/set-pin', setPin);
+userAuthRouter.post('/:userId/verify-pin', verifyPin);
 
 export default userAuthRouter;
