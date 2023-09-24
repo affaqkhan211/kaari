a482e06114b70e0301762421bf9c57c8945be234
affaqkhan211 <affaqkhan211@gmail.com>
2023-09-24 10:52:57 +0500

    login issue resolved

--- controllers/userAuthCotroller.js
+++ controllers/userAuthCotroller.js
@@ -6,23 +6,23 @@ import bcrypt from "bcrypt"
 export const userSignup = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
 
-  try {
-    if (!firstName || !lastName || !email || !password) {
-      return res.status(400).json({ message: 'All fields are required' });
-    }
+    try {
+        if (!firstName || !lastName || !email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
 
-    const existingUser = await UserAuth.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'Email is already in use' });
-    }
+        const existingUser = await UserAuth.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email is already in use' });
+        }
 
-    // Create a new user
-    const newUser = new UserAuth({ firstName, lastName, email, password });
-    await newUser.save();
+        // Create a new user
+        const newUser = new UserAuth({ firstName, lastName, email, password });
+        await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
-        res.status(500).json({ success : false,  message: 'Error creating user', error: error.message });
+        res.status(500).json({ success: false, message: 'Error creating user', error: error.message });
     }
 };
 
@@ -60,48 +60,44 @@ export const userSignup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
-  try {
-    if (!email || !password) {
-      return res.status(400).json({ message: 'Both email and password are required' });
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Both email and password are required' });
+        }
+
+        const user = await UserAuth.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+          }
+
+
+        const token = jwt.sign({ userId: user._id }, process.env.LOGIN_SECRET_KEY, { expiresIn: '3h' });
+
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-
-    const user = await UserAuth.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    // Compare password
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      return res.status(401).json({ message: 'Invalid email or password' });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.LOGIN_SECRET_KEY, { expiresIn: '3h' });
-
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
 };
 
 
 export const addAddress = async (req, res) => {
     const { address } = req.body;
 
-  try {
-    if (!address) {
-      return res.status(400).json({ message: 'Address is required' });
+    try {
+        if (!address) {
+            return res.status(400).json({ message: 'Address is required' });
+        }
+
+        const newAddress = new Address({ address });
+        await newAddress.save();
+
+        res.status(201).json({ message: 'Address added successfully', address: newAddress });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
-
-    const newAddress = new Address({ address });
-    await newAddress.save();
-
-    res.status(201).json({ message: 'Address added successfully', address: newAddress });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
 };
 
 export const setPin = async (req, res) => {
