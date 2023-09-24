af46a65787ce9f495c619629439ca1796762916f
affaqkhan211 <affaqkhan211@gmail.com>
2023-09-24 09:59:30 +0500

    cors issue resolved

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const app = express();
 import dotenv from "dotenv";
 import Connection from "./db/conn.js";
 import userAuthRouter from "./routes/userAuthRoutes.js";
+import cors from "cors";
 
 dotenv.config()
 const PORT = process.env.PORT ;
+app.use(cors());
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
 Connection()
 app.use(express.json())
 
